7f43102471a8d213d3bc82aaf52a0ffd1acb91c1
Sudip Dutta <suddutt1@in.ibm.com>
2019-07-25 11:54:46 +0530

    Updated the first transaction

--- main.go
+++ main.go
@@ -3,49 +3,86 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"strings"
+	"time"
 
 	"github.com/hyperledger/fabric/core/chaincode/shim"
+	id "github.com/hyperledger/fabric/core/chaincode/shim/ext/cid"
 	pb "github.com/hyperledger/fabric/protos/peer"
 )
 
-var _MAIN_LOGGER = shim.NewLogger("BaseSmartContractMain")
+var _MainLogger = shim.NewLogger("CarTrackLogger")
 
+//SmartContract for car tracking
 type SmartContract struct {
+}
+
+//CarDetails represents the car record to be stored in ledger
+type CarDetails struct {
+	ObjType       string `json:"objType"`
+	ChasisNumber  string `json:"chasisNumber"`
+	Manufacturer  string `json:"manufacturer"`
+	Year          string `json:"makeYear"`
+	Model         string `json:"model"`
+	Color         string `json:"color"`
+	LisenseNunber string `json:"licNumber"`
+	Status        string `json:"status"`
+	Dealer        string `json:"dealer"`
+	OwnerName     string `json:"owner"`
+	UpdateTs      string `json:"ts"`
+	TrxnID        string `json:"trxnId"`
+	UpdateBy      string `json:"updBy"`
 }
 
 // Init initializes chaincode.
 func (sc *SmartContract) Init(stub shim.ChaincodeStubInterface) pb.Response {
-	_MAIN_LOGGER.Infof("Inside the init method ")
+	_MainLogger.Infof("Inside the init method ")
 
 	return shim.Success(nil)
 }
 func (sc *SmartContract) probe(stub shim.ChaincodeStubInterface) pb.Response {
 	ts := ""
-	_MAIN_LOGGER.Info("Inside probe method")
+	_MainLogger.Info("Inside probe method")
 	tst, err := stub.GetTxTimestamp()
 	if err == nil {
 		ts = tst.String()
 	}
 	output := "{\"status\":\"Success\",\"ts\" : \"" + ts + "\" }"
-	_MAIN_LOGGER.Info("Retuning " + output)
+	_MainLogger.Info("Retuning " + output)
 	return shim.Success([]byte(output))
 }
 
-func (sc *SmartContract) save(stub shim.ChaincodeStubInterface) pb.Response {
+func (sc *SmartContract) createCarEntry(stub shim.ChaincodeStubInterface) pb.Response {
 	_, args := stub.GetFunctionAndParameters()
-	if len(args) < 2 {
+	if len(args) < 1 {
+		_MainLogger.Errorf("Invalid number of arguments")
 		return shim.Error("Invalid number of arguments")
 	}
-	key := args[0]
-	value := args[1]
-	txID := stub.GetTxID()
-	dataToStore := map[string]string{
-		"value":  value,
-		"trxnId": txID,
-		"id":     key,
+	var carDetails CarDetails
+	if err := json.Unmarshal([]byte(args[0]), &carDetails); err != nil {
+		_MainLogger.Errorf("Unable to parse the input car details JSON %v", err)
+		return shim.Error("Unable to parse the input car details JSON")
 	}
-	jsonBytesToStore, _ := json.Marshal(dataToStore)
-	stub.PutState(key, jsonBytesToStore)
+	idOk, manuf := sc.getInvokerIdentity(stub)
+	if !idOk {
+		return shim.Error("Unable to retrive the invoker ID")
+	}
+	if strings.TrimSpace(carDetails.ChasisNumber) == "" {
+		_MainLogger.Error("No chasis number provided")
+		return shim.Error("No chasis number provided")
+	}
+	carDetails.Manufacturer = manuf
+	carDetails.ObjType = "car"
+	carDetails.UpdateBy = manuf
+	carDetails.Status = "NEW"
+	carDetails.TrxnID = stub.GetTxID()
+	carDetails.UpdateTs = sc.getTrxnTS(stub)
+	jsonBytesToStore, _ := json.Marshal(carDetails)
+	//TODO: Check the chasis number
+	if err := stub.PutState(carDetails.ChasisNumber, jsonBytesToStore); err != nil {
+		_MainLogger.Errorf("Unable to store the car details %v", err)
+		return shim.Error("Unable to store the car details ")
+	}
 
 	return shim.Success([]byte(jsonBytesToStore))
 }
@@ -100,8 +137,8 @@ func (sc *SmartContract) Invoke(stub shim.ChaincodeStubInterface) pb.Response {
 	switch action {
 	case "probe":
 		response = sc.probe(stub)
-	case "save":
-		response = sc.save(stub)
+	case "createCarEntry":
+		response = sc.createCarEntry(stub)
 	case "saveKV":
 		response = sc.saveKV(stub)
 	case "query":
@@ -112,9 +149,28 @@ func (sc *SmartContract) Invoke(stub shim.ChaincodeStubInterface) pb.Response {
 	return response
 }
 
+func (sc *SmartContract) getInvokerIdentity(stub shim.ChaincodeStubInterface) (bool, string) {
+	//Following id comes in the format X509::<Subject>::<Issuer>>
+	enCert, err := id.GetX509Certificate(stub)
+	if err != nil {
+		return false, "Unknown."
+	}
+	return true, fmt.Sprintf("%s", enCert.Subject.CommonName)
+
+}
+func (sc *SmartContract) getTrxnTS(stub shim.ChaincodeStubInterface) string {
+	txTime, err := stub.GetTxTimestamp()
+	if err != nil {
+		return "0000.00.00.00.00.000"
+	}
+	var ts time.Time
+	newTS := ts.Add(time.Duration(txTime.Seconds) * time.Second)
+	return newTS.Format("2006.01.02.15.04.05.000")
+
+}
 func main() {
 	err := shim.Start(new(SmartContract))
 	if err != nil {
-		_MAIN_LOGGER.Criticalf("Error starting  chaincode: %v", err)
+		_MainLogger.Criticalf("Error starting  chaincode: %v", err)
 	}
 }
